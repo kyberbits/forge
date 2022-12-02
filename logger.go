5c31e3a2b6b471e31df1dc98aaa0028c007a5d8b
Aaron Ellington <aaron@ellington.io>
2022-12-02 12:25:56 +0000

    update logger

--- logger.go
+++ logger.go
@@ -14,12 +14,9 @@ type Log struct {
 }
 
 type Logger interface {
-	Emergency(message string, context map[string]interface{})
-	Alert(message string, context map[string]interface{})
 	Critical(message string, context map[string]interface{})
 	Error(message string, context map[string]interface{})
 	Warning(message string, context map[string]interface{})
-	Notice(message string, context map[string]interface{})
 	Info(message string, context map[string]interface{})
 	Debug(message string, context map[string]interface{})
 	StandardLogger() *log.Logger
@@ -27,14 +24,6 @@ type Logger interface {
 
 type LoggerJSON struct {
 	Encoder *json.Encoder
-}
-
-func (logger *LoggerJSON) Emergency(message string, context map[string]interface{}) {
-	logger.log("EMERGENCY", message, context)
-}
-
-func (logger *LoggerJSON) Alert(message string, context map[string]interface{}) {
-	logger.log("ALERT", message, context)
 }
 
 func (logger *LoggerJSON) Critical(message string, context map[string]interface{}) {
@@ -47,10 +36,6 @@ func (logger *LoggerJSON) Error(message string, context map[string]interface{}) 
 
 func (logger *LoggerJSON) Warning(message string, context map[string]interface{}) {
 	logger.log("WARNING", message, context)
-}
-
-func (logger *LoggerJSON) Notice(message string, context map[string]interface{}) {
-	logger.log("NOTICE", message, context)
 }
 
 func (logger *LoggerJSON) Info(message string, context map[string]interface{}) {
@@ -75,7 +60,7 @@ func (logger *LoggerJSON) log(severity string, message string, context map[strin
 }
 
 func (logger *LoggerJSON) Write(b []byte) (int, error) {
-	logger.Alert("Standard Library Log", map[string]interface{}{
+	logger.Error("Standard Library Log", map[string]interface{}{
 		"log": string(b),
 	})
 
