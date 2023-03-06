85f4fcbaf6b2b8202fc5cf688b41063bad987653
Aaron Ellington <aaron@ellington.io>
2023-03-06 11:57:09 -0500

    update static handler

--- forge/http_static.go
+++ forge/http_static.go
@@ -12,13 +12,18 @@ type HTTPStatic struct {
 	FileSystem      http.FileSystem
 	NotFoundHandler http.Handler
 	CacheControl    string
+	Index           string
 }
 
 func (httpStatic *HTTPStatic) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if httpStatic.Index == "" {
+		httpStatic.Index = "index.html"
+	}
+
 	requestedFileName := r.URL.Path
 	isRequestingDirectory := strings.HasSuffix(requestedFileName, "/")
 	if isRequestingDirectory {
-		requestedFileName += "index.html"
+		requestedFileName += httpStatic.Index
 	}
 
 	file, err := httpStatic.FileSystem.Open(requestedFileName)
@@ -27,12 +32,14 @@ func (httpStatic *HTTPStatic) ServeHTTP(w http.ResponseWriter, r *http.Request) 
 		return
 	}
 	defer file.Close()
+
 	fileTypeHeader := mime.TypeByExtension(filepath.Ext(requestedFileName))
 
 	w.Header().Set("Content-Type", fileTypeHeader)
 	if httpStatic.CacheControl != "" {
 		w.Header().Set("Cache-Control", httpStatic.CacheControl)
 	}
+
 	w.WriteHeader(http.StatusOK)
 	io.Copy(w, file)
 }
